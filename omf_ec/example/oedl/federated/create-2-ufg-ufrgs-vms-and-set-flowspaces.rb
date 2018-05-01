8d17d81afa3f819e5e8435660595aa038e302cff
viniciusgb4 <vinicius.esufg@gmail.com>
2018-05-01 12:29:56 -0300

    fix an oedl example

--- omf_ec/example/oedl/federated/create-2-ufg-ufrgs-vms-and-set-flowspaces.rb
+++ omf_ec/example/oedl/federated/create-2-ufg-ufrgs-vms-and-set-flowspaces.rb
@@ -35,19 +35,24 @@ onEvent(:ALL_VM_GROUPS_UP) do |ev|
   onEvent(:ALL_VMS_CREATED) do |ev_created|
     vm_ufg_ip = ""
     vm_ufrgs_ip = ""
-    while vm_ufg_ip.empty? or vm_ufrgs_ip.empty?
+    defEvent :IPS_GOT do
+      !vm_ufg_ip.empty? and !vm_ufrgs_ip.empty?
+    end
+    every(20) {
       vm_ufg.ip do |ip|
-        vm_ufg_ip = ip
+        info "UFG IP = #{ip}"
+        vm_ufg_ip = "#{ip}"
       end
       vm_ufrgs.ip do |ip|
-        vm_ufrgs_ip = ip
+        info "UFRGS IP = #{ip}"
+        vm_ufrgs_ip = "#{ip}"
       end
       puts "vm_ufrgs_ip = #{vm_ufrgs_ip}, vm_ufg_ip = #{vm_ufg_ip}"
-      sleep(20)
+    }
+    onEvent(:IPS_GOT) do |event|
+      puts "IPS GOT = #{vm_ufg_ip}"
+      define_flowvisor_slices(vm_ufg_ip)
     end
-    puts "CONFIGURING FLOWVISOR SLICES"
-    define_flowvisor_slices(vm_ufg_ip)
-    create_flowvisor_slices()
   end
 end
 
@@ -60,7 +65,7 @@ def define_flowvisor_slices(controller_ip)
 
   defFlowVisor('fv_noc', flowvisor_topic_noc) do |flowvisor|
     flowvisor.addSlice('slice1') do |slice1|
-      slice1.controller = "tcp:#{CONTROLLER_IP}:6633"
+      slice1.controller = "tcp:#{controller_ip}:6633"
       slice1.addFlow('noc1-ufg') do |flow1|
         flow1.operation = 'add'
         flow1.device = '00:00:00:04:df:61:5e:4d'
@@ -86,7 +91,7 @@ def define_flowvisor_slices(controller_ip)
 
   defFlowVisor('fv_ufrgs', flowvisor_topic_ufrgs) do |flowvisor|
     flowvisor.addSlice('slice1') do |slice1|
-      slice1.controller = "tcp:#{CONTROLLER_IP}:6633"
+      slice1.controller = "tcp:#{controller_ip}:6633"
       slice1.addFlow('ufrgs-noc2') do |flow1|
         flow1.operation = 'add'
         flow1.device = '67:8c:08:9e:01:a7:de:59'
@@ -102,7 +107,7 @@ def define_flowvisor_slices(controller_ip)
 
   defFlowVisor('fv_ufg', flowvisor_topic_ufg) do |flowvisor|
     flowvisor.addSlice('slice1') do |slice1|
-      slice1.controller = "tcp:#{CONTROLLER_IP}:6633"
+      slice1.controller = "tcp:#{controller_ip}:6633"
       slice1.addFlow('ufg-noc1') do |flow1|
         flow1.operation = 'add'
         flow1.device = '67:8c:08:9e:01:62:d6:42'
@@ -118,45 +123,43 @@ def define_flowvisor_slices(controller_ip)
 
 end
 
-def create_flowvisor_slices()
-  onEvent(:ALL_FLOWVISOR_UP) do |event|
-    info "Successfully subscribed on ALL Flowvisors RCs"
+onEvent(:ALL_FLOWVISOR_UP) do |event|
+  info "Successfully subscribed on ALL Flowvisors RCs"
 
-    info "Creating slices"
+  info "Creating slices"
 
-    flowvisor('fv_noc').createAllSlices
-    flowvisor('fv_ufrgs').createAllSlices
-    flowvisor('fv_ufg').createAllSlices
+  flowvisor('fv_noc').createAllSlices
+  flowvisor('fv_ufrgs').createAllSlices
+  flowvisor('fv_ufg').createAllSlices
 
-    onEvent(:ALL_SLICES_CREATED) do |ev_created|
-      info 'All slices created'
+  onEvent(:ALL_SLICES_CREATED) do |ev_created|
+    info 'All slices created'
 
-      info 'Installing Flows'
-      flowvisor('fv_noc').slice('slice1').installFlows do
-        info "Flows NOC installed"
-      end
-
-      flowvisor('fv_ufrgs').slice('slice1').installFlows do
-        info "Flows UFRGS installed"
-      end
-
-      flowvisor('fv_ufg').slice('slice1').installFlows do
-        info "Flows UFG installed"
-      end
-
-
-      after(60) {
-        info "Removing openflow flows..."
-        flowvisor('fv_noc').releaseAllSlices
-        flowvisor('fv_ufrgs').releaseAllSlices
-        flowvisor('fv_ufg').releaseAllSlices
-      }
-
-      after(70) {
-        Experiment.done
-      }
-
+    info 'Installing Flows'
+    flowvisor('fv_noc').slice('slice1').installFlows do
+      info "Flows NOC installed"
     end
 
+    flowvisor('fv_ufrgs').slice('slice1').installFlows do
+      info "Flows UFRGS installed"
+    end
+
+    flowvisor('fv_ufg').slice('slice1').installFlows do
+      info "Flows UFG installed"
+    end
+
+
+    after(600) {
+      info "Removing openflow flows..."
+      flowvisor('fv_noc').releaseAllSlices
+      flowvisor('fv_ufrgs').releaseAllSlices
+      flowvisor('fv_ufg').releaseAllSlices
+    }
+
+    after(610) {
+      Experiment.done
+    }
+
   end
+
 end
